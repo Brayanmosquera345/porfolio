471a386688971c82b738b5c8046118a92e81d7aa
Brayanmosquera345 <davidmosquera360@gmail.com>
2025-08-13 11:17:20 -0500

    feat(frontend) Se cambio el todo el diseño del portafolio, ademas se separo en secciones para que sea mas mantenible

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        background: "#F6F6F6",
+        primary: "#070707",
+        paragraph: "#6A6A6A",
+        accent: {
+          DEFAULT: "#769FD8",
+          100: "#DCEAFE",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
